624e41b43f70b992ff20ba9b75dc7075fa3efe6d
João Pedro Menegali Salvan Bitencourt <joao.ms@aluno.ifsc.edu.br>
2023-12-16 13:21:28 -0300

    Adicionada consulta por nome.

--- SistemaLobinho/src/main/java/engtelecom/bcd/repository/DistintivosRepository.java
+++ SistemaLobinho/src/main/java/engtelecom/bcd/repository/DistintivosRepository.java
@@ -3,5 +3,8 @@ package engtelecom.bcd.repository;
 import engtelecom.bcd.model.Distintivos;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface DistintivosRepository extends CrudRepository<Distintivos, Integer> {
+    Optional<Distintivos> findDistintivosByNome(String nomeDistintivo);
 }
