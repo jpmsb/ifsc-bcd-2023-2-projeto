b93ec2fda97ccdbfe33aa08e4009d0a698408af0
João Pedro Menegali Salvan Bitencourt <joao.ms@aluno.ifsc.edu.br>
2023-12-17 18:45:54 -0300

    Feita a lógica para listar jovens que possuam uma determinada especialidade.

--- SistemaLobinho/src/main/java/engtelecom/bcd/SistemaLobinhoApplication.java
+++ SistemaLobinho/src/main/java/engtelecom/bcd/SistemaLobinhoApplication.java
@@ -1,10 +1,7 @@
 package engtelecom.bcd;
 
-import engtelecom.bcd.model.Pessoa;
-import engtelecom.bcd.model.ProblemasDeSaude;
-import engtelecom.bcd.model.Responsavel;
-import engtelecom.bcd.repository.PessoaRepository;
-import engtelecom.bcd.repository.TipoSanguineoRepository;
+import engtelecom.bcd.model.*;
+import engtelecom.bcd.repository.*;
 import lombok.NonNull;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
@@ -13,6 +10,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.Scanner;
 import java.util.Set;
 
@@ -24,7 +23,36 @@ public class SistemaLobinhoApplication {
 
 	@NonNull
 	@Autowired
-	private TipoSanguineoRepository tipoSanguineoRepository;
+	private EspecialidadesRepository especialidadesRepository;
+
+	@NonNull
+	@Autowired
+	private InsigniasRepository insigniasRepository;
+
+	@NonNull
+	@Autowired
+	private DistintivosRepository distintivosRepository;
+
+	@NonNull
+	@Autowired
+	private AcampamentosRepository acampamentosRepository;
+
+	@NonNull
+	@Autowired
+	private AtividadesDeEspecialidadesFeitasRepository atividadesDeEspecialidadesFeitasRepository;
+
+	@NonNull
+	@Autowired
+	private AtividadesDeEspecialidadesRepository atividadesDeEspecialidadesRepository;
+
+	@NonNull
+	@Autowired
+	private AtividadesDeInsigniasRepository atividadesDeInsigniasRepository;
+
+	@NonNull
+	@Autowired
+	private AtividadesDeDistintivosFeitasRepository atividadesDeDistintivosFeitasRepository;
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(SistemaLobinhoApplication.class, args);
@@ -43,15 +71,9 @@ public class SistemaLobinhoApplication {
 		Scanner entrada = new Scanner(System.in);
 		boolean sair = false;
 
-		System.out.println("Bem-vindo(a) ao Sistema Lobinho!!");
-		System.out.println("Escolha dentre as opções abaixo para interagir com o sistema:\n");
-
 		String opcoes[] = {
-			"Listar dados bibliográficos de um jovem.",
-			"Listar jovens com uma determinada especialidades.",
-			"Listar as especialidades e insígnias de interesse especial que um determinado jovem possui.",
-			"Listar os requisitos já cumpridos por um determinado jovem para uma determinada especialidade.",
-			"Listar jovens que possuem todos os requisitos para obter o Cruzeiro do Sul."
+				"Cadastro.",
+				"Gerar relatório."
 		};
 
 		exibirOpcoes(opcoes);
@@ -64,10 +86,47 @@ public class SistemaLobinhoApplication {
 					sair = true;
 					break;
 				case 1:
-					listarDadosBibliograficos();
-					exibirOpcoes(opcoes);
+					menuCadastrar();
 					break;
 				case 2:
+					menuGerarRelatorio();
+					break;
+				default:
+					System.out.println("\nOpção inválida! Escolha dentre as opções abaixo:\n");
+			}
+			exibirOpcoes(opcoes);
+		}
+		System.exit(0);
+	}
+
+
+	public void menuGerarRelatorio(){
+		Scanner entrada = new Scanner(System.in);
+		boolean sair = false;
+
+		String opcoes[] = {
+			"Listar dados bibliográficos de um jovem.",
+			"Listar jovens com uma determinada especialidades.",
+			"Listar as especialidades e insígnias de interesse especial que um determinado jovem possui.",
+			"Listar os requisitos já cumpridos por um determinado jovem para uma determinada especialidade.",
+			"Listar jovens que possuem todos os requisitos para obter o Cruzeiro do Sul."
+		};
+
+		while (! sair) {
+			exibirOpcoes(opcoes);
+			System.out.print("\nEscolha a opção desejada (1 .. " + opcoes.length + ", 0 para sair): ");
+			int opcao = entrada.nextInt();
+
+			switch (opcao) {
+				case 0:
+					sair = true;
+					System.out.println("Voltando ao menu anterior.");
+					break;
+				case 1:
+					listarDadosBibliograficos();
+					break;
+				case 2:
+					listarJovensComDeterminadaEspecialidade();
 					break;
 				case 3:
 					break;
@@ -76,26 +135,103 @@ public class SistemaLobinhoApplication {
 				case 5:
 					break;
 				default:
-					System.out.println("Opção inválida!");
+					System.out.println("\nOpção inválida! Escolha dentre as opções abaixo:\n");
 			}
+
 		}
-		System.exit(0);
 	}
 
-	private int exibirOpcoes(Iterable<Pessoa> pessoas){
+	private void menuCadastrar(){
+		Scanner entrada = new Scanner(System.in);
+		boolean sair = false;
+
+		String opcoes[] = {
+				"Cadastrar novo lobinho.",
+				"Registrar progresso."
+		};
+
+		while (! sair) {
+			exibirOpcoes(opcoes);
+			System.out.print("\nEscolha a opção desejada (1 .. " + opcoes.length + ", 0 para sair): ");
+			int opcao = entrada.nextInt();
+
+			switch (opcao) {
+				case 0:
+					sair = true;
+					System.out.println("Voltando ao menu anterior.");
+					break;
+				case 1:
+					 cadastrarNovoLobinho();
+					break;
+				case 2:
+					 registrarProgresso();
+					break;
+				default:
+					System.out.println("\nOpção inválida! Escolha dentre as opções abaixo:\n");
+			}
+
+		}
+	}
+
+	private void cadastrarNovoLobinho(){
+
+	}
+
+	private void registrarProgresso(){
+
+	}
+
+	private int exibirPessoas(){
 		int numeroOpcao = 0;
-		for (Pessoa pessoa : pessoas){
-			System.out.println(numeroOpcao + ". " + pessoa.getNome());
+		for (Pessoa pessoa : pessoaRepository.findAll()){
 			numeroOpcao++;
+			System.out.println(numeroOpcao + ". " + pessoa.getNome());
 		}
 		return numeroOpcao;
 	}
 
+	private int exibirEspecialidades(){
+		int numeroOpcao = 0;
+		for (Especialidades especialidade : especialidadesRepository.findAll()){
+			numeroOpcao++;
+			System.out.println(numeroOpcao + ". " + especialidade.getNome());
+		}
+		return numeroOpcao;
+	}
+
+	private int exibirInsignias(){
+		int numeroOpcao = 0;
+		for (Insignias insignia : insigniasRepository.findAll()){
+			numeroOpcao++;
+			System.out.println(numeroOpcao + ". " + insignia.getNome());
+		}
+		return numeroOpcao;
+	}
+
+	private int exibirDistintivos(){
+		int numeroOpcao = 0;
+		for (Distintivos distintivo : distintivosRepository.findAll()){
+			numeroOpcao++;
+			System.out.println(numeroOpcao + ". " + distintivo.getNome());
+		}
+		return numeroOpcao;
+	}
+
+	private int exibirAcampamentos(){
+		int numeroOpcao = 0;
+		for (Acampamentos acampamento : acampamentosRepository.findAll()){
+			numeroOpcao++;
+			System.out.println(numeroOpcao + ". " + acampamento.getNome());
+		}
+		return numeroOpcao;
+	}
+
+
 	private int exibirOpcoes(String itens[]){
 		int numeroOpcao = 0;
 		for (String item : itens){
-			System.out.println(numeroOpcao + ". " + item);
 			numeroOpcao++;
+			System.out.println(numeroOpcao + ". " + item);
 		}
 		return numeroOpcao;
 	}
@@ -104,19 +240,18 @@ public class SistemaLobinhoApplication {
 		Scanner entrada = new Scanner(System.in);
 		boolean sair = false;
 
+		System.out.println("\n\nSelecione um jovem:\n");
 
-		System.out.println("Selecione um jovem:\n");
-
-		int numeroOpcao = exibirOpcoes(pessoaRepository.findAll());
+		int quantidadeOpcoes = exibirPessoas();
 
 		while (! sair) {
-			System.out.print("\nEscolha a opção desejada (1 .. " + numeroOpcao + ", 0 para sair): ");
+			System.out.print("\nEscolha a opção desejada (1 .. " + quantidadeOpcoes + ", 0 para sair): ");
 			int opcao = entrada.nextInt();
 
 			if (opcao == 0) {
 				System.out.println("Voltando ao menu anterior.");
 				sair = true;
-			} else if (opcao < 1 || opcao > numeroOpcao) {
+			} else if (opcao < 1 || opcao > quantidadeOpcoes) {
 				System.out.println("Opção inválida!");
 			} else {
 				var consulta = pessoaRepository.findById(opcao);
@@ -160,6 +295,87 @@ public class SistemaLobinhoApplication {
 							System.out.println(" - E-mail:   " + responsavel.getEmail());
 						}
 					}
+				}
+			}
+		}
+	}
+
+	private void listarJovensComDeterminadaEspecialidade(){
+		Scanner entrada = new Scanner(System.in);
+		boolean sair = false;
+
+		System.out.println("\n\nSelecione uma especialidade:\n");
+
+		int quantidadeOpcoes = exibirEspecialidades();
+
+		while (! sair) {
+			System.out.print("\nEscolha a opção desejada (1 .. " + quantidadeOpcoes + ", 0 para sair): ");
+			int opcao = entrada.nextInt();
+
+			if (opcao == 0) {
+				System.out.println("Voltando ao menu anterior.");
+				sair = true;
+			} else if (opcao < 1 || opcao > quantidadeOpcoes) {
+				System.out.println("Opção inválida!");
+			} else {
+				// Atividades da especialidade escolhida
+				var consultaEspecialidade = especialidadesRepository.findById(opcao);
+				var objetoEspecialidade = consultaEspecialidade.get();
+				var atividades = atividadesDeEspecialidadesRepository.findAllByEspecialidadeOrderByIdAtividade(objetoEspecialidade);
+				int quantidadeTotalDeAtividades = atividades.size();
+
+				int indiceNivel1 = (quantidadeTotalDeAtividades / 3) - 1;
+				int indiceNivel2 = ((quantidadeTotalDeAtividades * 2) / 3) - 1;
+				int indiceNivel3 = quantidadeTotalDeAtividades - 1;
+
+				var atividadesFeitas = atividadesDeEspecialidadesFeitasRepository.findAll();
+
+				// Obtendo as pessoas que fizeram alguma atividade
+				Set<Pessoa> pessoas = new HashSet<>();
+				for (AtividadesDeEspecialidadesFeitas atividadePessoa : atividadesFeitas){
+					pessoas.add(atividadePessoa.getPessoa());
+				}
+
+				System.out.println();
+
+				// Verificando em cada pessoa as atividades da especialidade escolhida
+				for (Pessoa pessoa : pessoas){
+					ArrayList<AtividadesDeEspecialidades> atividadesRealizadas = new ArrayList<>();
+
+					var atividadesFeitasPelaPessoa = atividadesDeEspecialidadesFeitasRepository.findAllByPessoaOrderByData(pessoa);
+
+					for (AtividadesDeEspecialidadesFeitas atividadeFeitaPelaPessoa : atividadesFeitasPelaPessoa){
+						if (atividadeFeitaPelaPessoa.getAtividadeDeEspecialidade().getEspecialidade().equals(objetoEspecialidade)){
+							atividadesRealizadas.add(atividadeFeitaPelaPessoa.getAtividadeDeEspecialidade());
+						}
+					}
+
+					System.out.println(pessoa.getNome() + " possui a especialidade " + objetoEspecialidade.getNome() + ": ");
+
+					if (atividadesRealizadas.size() >= indiceNivel1+1) {
+						AtividadesDeEspecialidades atividadeNivel1 = atividadesRealizadas.get(indiceNivel1);
+
+						LocalDate dataNivel1 = atividadesDeEspecialidadesFeitasRepository.findAtividadesDeEspecialidadesFeitasByPessoaAndAtividadeDeEspecialidade(pessoa, atividadeNivel1).getData().toLocalDate();
+
+						System.out.println(" - Nível 1: " + dataNivel1.getDayOfMonth() + "/" + dataNivel1.getMonthValue() + "/" + dataNivel1.getYear());
+					}
+
+					if (atividadesRealizadas.size() >= indiceNivel2+1) {
+						AtividadesDeEspecialidades atividadeNivel2 = atividadesRealizadas.get(indiceNivel2);
+
+						LocalDate dataNivel2 = atividadesDeEspecialidadesFeitasRepository.findAtividadesDeEspecialidadesFeitasByPessoaAndAtividadeDeEspecialidade(pessoa, atividadeNivel2).getData().toLocalDate();
+
+						System.out.println(" - Nível 2: " + dataNivel2.getDayOfMonth() + "/" + dataNivel2.getMonthValue() + "/" + dataNivel2.getYear());
+					}
+
+					if (quantidadeTotalDeAtividades == atividadesRealizadas.size()) {
+						AtividadesDeEspecialidades atividadeNivel3 = atividadesRealizadas.get(indiceNivel3);
+
+						LocalDate dataNivel3 = atividadesDeEspecialidadesFeitasRepository.findAtividadesDeEspecialidadesFeitasByPessoaAndAtividadeDeEspecialidade(pessoa, atividadeNivel3).getData().toLocalDate();
+
+						System.out.println(" - Nível 3: " + dataNivel3.getDayOfMonth() + "/" + dataNivel3.getMonthValue() + "/" + dataNivel3.getYear());
+					}
+					System.out.println();
 				}
 			}
 		}
