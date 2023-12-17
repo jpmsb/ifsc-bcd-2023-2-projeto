8f34002716c6657d09af98295e324e5412fb7f1f
João Pedro Menegali Salvan Bitencourt <joao.ms@aluno.ifsc.edu.br>
2023-12-17 20:17:04 -0300

    Adicionada consulta para obter uma lista com todas as atividades de uma insígnia.

--- SistemaLobinho/src/main/java/engtelecom/bcd/repository/AtividadesDeInsigniasRepository.java
+++ SistemaLobinho/src/main/java/engtelecom/bcd/repository/AtividadesDeInsigniasRepository.java
@@ -1,7 +1,13 @@
 package engtelecom.bcd.repository;
 
+import engtelecom.bcd.model.AtividadesDeEspecialidades;
 import engtelecom.bcd.model.AtividadesDeInsignias;
+import engtelecom.bcd.model.Especialidades;
+import engtelecom.bcd.model.Insignias;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.ArrayList;
+
 public interface AtividadesDeInsigniasRepository extends CrudRepository<AtividadesDeInsignias, Integer> {
+    ArrayList<AtividadesDeInsignias> findAllByInsigniaOrderByIdAtividade(Insignias insignia);
 }
