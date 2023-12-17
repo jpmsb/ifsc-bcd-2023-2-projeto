941d86a069d3612352380c7f8c05b5c11cf94cb0
João Pedro Menegali Salvan Bitencourt <joao.ms@aluno.ifsc.edu.br>
2023-12-17 20:16:03 -0300

    Adicionadas consultas para obter a lista de atividades de insígnia realizadas por pessoa

--- SistemaLobinho/src/main/java/engtelecom/bcd/repository/AtividadesDeInsigniasFeitasRepository.java
+++ SistemaLobinho/src/main/java/engtelecom/bcd/repository/AtividadesDeInsigniasFeitasRepository.java
@@ -1,7 +1,12 @@
 package engtelecom.bcd.repository;
 
-import engtelecom.bcd.model.AtividadesDeInsigniasFeitas;
+import engtelecom.bcd.model.*;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.ArrayList;
+
 public interface AtividadesDeInsigniasFeitasRepository extends CrudRepository<AtividadesDeInsigniasFeitas, Integer> {
+    ArrayList<AtividadesDeInsigniasFeitas> findAllByPessoaOrderByData(Pessoa pessoa);
+
+    AtividadesDeInsigniasFeitas findAtividadesDeInsigniasFeitasByPessoaAndAtividadeDeInsignia(Pessoa pessoa, AtividadesDeInsignias atividadeDeInsignia);
 }
