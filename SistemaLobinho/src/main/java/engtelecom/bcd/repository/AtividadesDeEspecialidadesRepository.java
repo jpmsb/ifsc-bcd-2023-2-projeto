6087a59e163e521c17df57c4528de9b557ebd5a9
João Pedro Menegali Salvan Bitencourt <joao.ms@aluno.ifsc.edu.br>
2023-12-17 18:48:13 -0300

    Criada a consulta que retorna a atividade baseada na especialidade ordenando pelo id da atividade.

--- SistemaLobinho/src/main/java/engtelecom/bcd/repository/AtividadesDeEspecialidadesRepository.java
+++ SistemaLobinho/src/main/java/engtelecom/bcd/repository/AtividadesDeEspecialidadesRepository.java
@@ -1,7 +1,12 @@
 package engtelecom.bcd.repository;
 
 import engtelecom.bcd.model.AtividadesDeEspecialidades;
+import engtelecom.bcd.model.Especialidades;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.ArrayList;
+import java.util.Optional;
+
 public interface AtividadesDeEspecialidadesRepository extends CrudRepository<AtividadesDeEspecialidades, Integer> {
+    ArrayList<AtividadesDeEspecialidades> findAllByEspecialidadeOrderByIdAtividade(Especialidades especialidades);
 }
