0677d347236c87d5e3840516c962d69e1f87d552
João Pedro Menegali Salvan Bitencourt <joao.ms@aluno.ifsc.edu.br>
2023-12-17 18:47:14 -0300

    Criadas consultas para retornar as atividades que um jovem fez de forma que fosse possível obter informações sobre a tarefa.

--- SistemaLobinho/src/main/java/engtelecom/bcd/repository/AtividadesDeEspecialidadesFeitasRepository.java
+++ SistemaLobinho/src/main/java/engtelecom/bcd/repository/AtividadesDeEspecialidadesFeitasRepository.java
@@ -1,7 +1,18 @@
 package engtelecom.bcd.repository;
 
+import engtelecom.bcd.model.AtividadesDeEspecialidades;
 import engtelecom.bcd.model.AtividadesDeEspecialidadesFeitas;
+import engtelecom.bcd.model.Pessoa;
 import org.springframework.data.repository.CrudRepository;
 
+import java.sql.Date;
+import java.util.ArrayList;
+import java.util.Optional;
+
 public interface AtividadesDeEspecialidadesFeitasRepository extends CrudRepository<AtividadesDeEspecialidadesFeitas, Integer> {
+    Iterable<AtividadesDeEspecialidadesFeitas> findAllByPessoaAndAtividadeDeEspecialidade(Pessoa pessoa, AtividadesDeEspecialidades atividadeDeEspecialidade);
+    Iterable<AtividadesDeEspecialidadesFeitas> findAllByPessoa(Pessoa pessoa);
+
+    ArrayList<AtividadesDeEspecialidadesFeitas> findAllByPessoaOrderByData(Pessoa pessoa);
+    AtividadesDeEspecialidadesFeitas findAtividadesDeEspecialidadesFeitasByPessoaAndAtividadeDeEspecialidade(Pessoa pessoa, AtividadesDeEspecialidades atividadeDeEspecialidade);
 }
