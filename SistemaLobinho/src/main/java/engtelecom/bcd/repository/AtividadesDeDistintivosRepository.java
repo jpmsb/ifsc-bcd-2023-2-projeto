6bd8bad8fe150ed3bcb48531ac78d1c6d2973e13
João Pedro Menegali Salvan Bitencourt <joao.ms@aluno.ifsc.edu.br>
2023-12-17 23:56:43 -0300

    Adicionada a consulta para obter as atividades.

--- SistemaLobinho/src/main/java/engtelecom/bcd/repository/AtividadesDeDistintivosRepository.java
+++ SistemaLobinho/src/main/java/engtelecom/bcd/repository/AtividadesDeDistintivosRepository.java
@@ -1,7 +1,11 @@
 package engtelecom.bcd.repository;
 
 import engtelecom.bcd.model.AtividadesDeDistintivos;
+import engtelecom.bcd.model.Distintivos;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.ArrayList;
+
 public interface AtividadesDeDistintivosRepository extends CrudRepository<AtividadesDeDistintivos, Integer> {
+    ArrayList<AtividadesDeDistintivos> findAllByDistintivoOrderByIdAtividade(Distintivos distintivo);
 }
